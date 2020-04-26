73cf6344b5d7c9cc6a46434ddb6aa938f197bdf2
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2020-04-26 16:35:19 +0900

    Add test

---
+++ index.spec.js
@@ -0,0 +1,46 @@
+const unified = require("unified");
+const markdown = require("remark-parse");
+const stringify = require("remark-stringify");
+const contents = require("./");
+
+const processor = unified()
+  .use(markdown, { commonmark: true })
+  .use(contents)
+  .use(stringify, { commonmark: true })
+  .freeze();
+
+describe("test", () => {
+  it("basic", () => {
+    return processor()
+      .process(
+        `# Alpha
+
+aaaa
+
+## Bravo
+
+bbbb
+
+### Charlie
+
+cccc
+
+## Delta
+
+dddd
+`
+      )
+      .then((res) => {
+        expect(res.contents).toBe(
+          `-   [Alpha](#alpha)
+
+    -   [Bravo](#bravo)
+
+        -   [Charlie](#charlie)
+
+    -   [Delta](#delta)
+`
+        );
+      });
+  });
+});
